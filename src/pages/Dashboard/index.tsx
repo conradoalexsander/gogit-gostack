e9b78ebaaa8a4bd6f4827d3e391201940a57a878
Conrado Alexsander <conradoalexsander@hotmail.com>
2020-07-03 19:31:09 -0300

    App completed

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,54 +1,101 @@
-import React from 'react';
-import logoImg from '../../assets/logo.svg';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
-import { Title, Form, Repositories } from './styles';
+import { Link } from 'react-router-dom';
+import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
+import { Title, Form, Repositories, Error } from './styles';
 
+// não é necessário colocar toda a tipagem do objeto retornado na requisição
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+    if (storedRepositories) {
+      return JSON.parse(storedRepositories);
+    }
+    return [];
+  });
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logoImg} alt="Github Explorer" />
-    <Title>Explore repositórios no Github </Title>
-    <Form>
-      <input placeholder="Digite aqui"></input>
-      <button type="submit">Pesquisar</button>
-    </Form>
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
 
-    <Repositories>
-      <a href="teste">
-        <img src="https://avatars1.githubusercontent.com/u/53683786?s=460&u=9a806b10abd688deee28c7a5dfd9e7e59cccf2ff&v=4"
-          alt="Conrado Alexsander" />
-        <div>
-          <strong>Rocketseat/unform</strong>
-          <p>Melhor que nós temos</p>
-        </div>
+  async function handleRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
 
-        <FiChevronRight size={20} />
-      </a>
-      <a href="teste">
-        <img src="https://avatars1.githubusercontent.com/u/53683786?s=460&u=9a806b10abd688deee28c7a5dfd9e7e59cccf2ff&v=4"
-          alt="Conrado Alexsander" />
-        <div>
-          <strong>Rocketseat/unform</strong>
-          <p>Melhor que nós temos</p>
-        </div>
+    try {
+      const response = await api.get(`repos/${newRepo}`);
 
-        <FiChevronRight size={20} />
-      </a>
-      <a href="teste">
-        <img src="https://avatars1.githubusercontent.com/u/53683786?s=460&u=9a806b10abd688deee28c7a5dfd9e7e59cccf2ff&v=4"
-          alt="Conrado Alexsander" />
-        <div>
-          <strong>Rocketseat/unform</strong>
-          <p>Melhor que nós temos</p>
-        </div>
+      const repository = response.data;
 
-        <FiChevronRight size={20} />
-      </a>
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch {
+      setInputError('Erro na busca por esse repositório');
+    }
+  }
 
-    </Repositories>
-  </>
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github </Title>
+      {/*
+Utilizando truhy ou falsy para converter o valor em booleano
+! = false, !! true */}
+      <Form hasError={!!inputError} onSubmit={handleRepository}>
+        <input
+          placeholder="Digite aqui"
+          onChange={e => setNewRepo(e.target.value)}
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
 
-)
+      {/* condicionando erro a somente aparecer caso exista (equivale a if/else) */}
+      {inputError && <Error>{inputError}</Error>}
+
+      <Repositories>
+        {repositories.map(repository => (
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,9 @@ import Repository from '../pages/Repository';
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
-    <Route path="/repository" component={Repository} />
+    {/* o + depois da barra indica que toda a string depois da barra faz parte do nome
+    da rota. Assim, o //rocketseat funcionaria, nesse app */}
+    <Route path="/repositories/:repository+" component={Repository} />
   </Switch>
 );
 
